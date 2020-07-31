dcbec02af5a97fc2885d7760e1e7282ec8f3c1e1
Sanjay Krishna <skp68@mst.edu>
2020-07-30 20:27:25 -0700

    binary search first two

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -17,11 +17,37 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
+        #check if new node value is less than current node value
+        if value < self.value:
+            if not self.left: #meaning if it is greater than
+                self.left = BSTNode(value)
+            else:
+                self.left.insert(value)
+        #check whether new node value is > or  = to current node value
+        elif value >= self.value:
+            if not self.right:
+                self.right = BSTNode(value)
+            else:
+                self.right.insert(value)
         pass
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
+        #check whether current node matches target
+        if self.value == target:
+            return True
+        if target < self.value:
+            if not self.left:
+                return False
+            else:
+                return self.left.contains(target)
+        else:
+            if not self.right:
+                return False
+            else:
+                return self.right.contains(target)
+                
         pass
 
     # Return the maximum value found in the tree
